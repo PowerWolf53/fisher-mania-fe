c223a2d140dd652d7d700bb31998a65798ef6ded
Maksim <maksim.borovoj@gmail.com>
2025-02-17 19:29:20 +0300

    first commit

---
+++ src/app/common-components/desktop/header/HeaderDesktop.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <span>Fisher Mania</span>
+        </div>
+    )
+}
---
+++ src/app/pages/desktop/landing/desktop/LandingDesktop.tsx
@@ -0,0 +1,18 @@
+import './landing-desktop.css'
+
+export default function LandingDesktop() {
+    return (
+        <div className="welcome-desktop">
+            <div className="home-image-desktop">
+            </div>
+            <div className='landing-text-wrapper-desktop'>
+                <p className="landing-welcome-text-desktop">Добро пожаловать на <br/>
+                    Fisher Mania <br/>
+                </p>
+                <span className='lending-sub-logo-desktop'>Ваш надёжный выбор рыболовных товаров</span>
+                <div className='more-button-desktop'>Узнать больше</div>
+            </div>
+        </div>
+    );
+
+}
