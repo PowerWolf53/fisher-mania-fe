8609291fbd268d5c2ed254d3873c1bc35b661d85
Maksim <maksim.borovoj@gmail.com>
2025-02-19 18:36:46 +0300

    master: remove field

--- src/app/pages/desktop/landing/desktop/contact-form-modal/ContactFormModal.tsx
+++ src/app/pages/desktop/landing/desktop/contact-form-modal/ContactFormModal.tsx
@@ -36,9 +36,9 @@ export default function ContactFormModal(
                                 Ваше имя *
                             </label>
                             <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                 id="grid-first-name" type="text" placeholder="Представьтесь пожалуйста"/>
-                            <p className="text-red-500 text-xs italic">Пожалуйста заполните это поле</p>
+                            {/*<p className="text-red-500 text-xs italic">Пожалуйста заполните это поле</p>*/}
                         </div>
                         <div className="w-full md:w-1/2 px-3">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -63,15 +63,7 @@ export default function ContactFormModal(
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-2">
-                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                   htmlFor="grid-city">
-                                Город
-                            </label>
-                            <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                id="grid-city" type="text" placeholder="Москва"/>
-                        </div>
+
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                    htmlFor="grid-state">
