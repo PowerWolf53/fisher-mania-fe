cf0cf300391772aa8edbc455ab9598908d14fa70
Maksim <maksim.borovoj@gmail.com>
2025-08-01 21:15:27 +0300

    master: remove index imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/app/common/components/Header";
 import Landing from "@/app/pages/landing/Landing";
+import Header from "@/app/common/Header/Header";
 
 
 export default function Home() {
