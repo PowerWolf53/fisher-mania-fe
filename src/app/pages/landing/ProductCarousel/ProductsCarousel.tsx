587f0853f2586f2acdfab0a207de4bec2f232b2f
Maksim <maksim.borovoj@gmail.com>
2025-08-01 21:02:42 +0300

    master: fix header

--- src/app/pages/landing/ProductCarousel/ProductsCarousel.tsx
+++ src/app/pages/landing/ProductCarousel/ProductsCarousel.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import './ProductsCarousel.css'
-import ProductCardDesktop
-    from "@/app/pages/landing/ProductCarousel/CarouselCard/CarouselCard";
+import CarouselCard from "./CarouselCard";
 import ProductCircle from '../../../../assets/images/main/products/circle.jpg'
 import Kolobaska from '../../../../assets/images/main/products/kolobaska.jpg'
 import Kobilka from '../../../../assets/images/main/products/kobilka.jpg'
@@ -15,10 +14,10 @@ export default function ProductsCarousel(){
         <div className='products-section-desktop'>
             <span className='products-section-label-desktop'>Ассортимент</span>
             <div className='products-slider-desktop'>
-                <ProductCardDesktop imgSrc={ProductCircle} title={'Кружок рыболовный'} description={'145мм-170мм неоснащённые/оснащённые: леска 10 метров 0.3-0.4'} />
-                <ProductCardDesktop imgSrc={Kolobaska} title={'Колобашка'} description={'Колобашка неоснащённая/оснащённая: леска 15 метров 0.3-0.4'} />
-                <ProductCardDesktop imgSrc={Kobilka} title={'Удочка кобылка'} description={'Удочка зимняя "Кобылка" с хлыстом неоснащённая'} />
-                <ProductCardDesktop imgSrc={Motilnica} title={'Мотыльница'} description={'Мотыльница зимняя красная/белая 3.5x10.5x7.5 см'} />
+                <CarouselCard imgSrc={ProductCircle} title={'Кружок рыболовный'} description={'145мм-170мм неоснащённые/оснащённые: леска 10 метров 0.3-0.4'} />
+                <CarouselCard imgSrc={Kolobaska} title={'Колобашка'} description={'Колобашка неоснащённая/оснащённая: леска 15 метров 0.3-0.4'} />
+                <CarouselCard imgSrc={Kobilka} title={'Удочка кобылка'} description={'Удочка зимняя "Кобылка" с хлыстом неоснащённая'} />
+                <CarouselCard imgSrc={Motilnica} title={'Мотыльница'} description={'Мотыльница зимняя красная/белая 3.5x10.5x7.5 см'} />
             </div>
         </div>
     )
